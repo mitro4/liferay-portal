c596f21846b0898120b051dbf24a6d098765463a
papa-liferay <papa@liferay.ru>
2016-10-25 15:58:10 +0300

    LRE-105 увеличил версию до 6.2.5.2

--- portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
+++ portal-service/src/com/liferay/portal/kernel/util/ReleaseInfo.java
@@ -98,6 +98,9 @@ public class ReleaseInfo {
 	public static final int RELEASE_6_2_10_BUILD_NUMBER = 6210;
 
 	public static final int RELEASE_6_2_5_1_BUILD_NUMBER = 62051;
+	
+	public static final int RELEASE_6_2_5_2_BUILD_NUMBER = 62052;
+	
 
 	public static final Date getBuildDate() {
 		DateFormat df = DateFormat.getDateInstance(DateFormat.LONG);
@@ -148,13 +151,13 @@ public class ReleaseInfo {
 		return _VERSION;
 	}
 
-	private static final String _BUILD = "62051";
+	private static final String _BUILD = "62052";
 
 	private static final int _BUILD_NUMBER = GetterUtil.getInteger(_BUILD);
 
 	private static final String _CODE_NAME = "Newton";
 
-	private static final String _DATE = "January 21, 2016";
+	private static final String _DATE = "October 25, 2016";
 
 	private static final String _NAME = "Liferay Portal Russian Edition";
 
@@ -168,7 +171,7 @@ public class ReleaseInfo {
 
 	private static final String _VENDOR = "Liferay, Inc.";
 
-	private static final String _VERSION = "6.2.5.1";
+	private static final String _VERSION = "6.2.5.2";
 
 	private static final String _VERSION_DISPLAY_NAME = "6.2 RE GA6";
 
