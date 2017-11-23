b635a2527d8f7996d229d0a9f6cb58621818271a
acdyukov <a.c.dyukov@gmail.com>
2017-11-23 11:33:53 +0700

    LRE-277: fix

--- portal-web/docroot/html/js/liferay/pagination.js
+++ portal-web/docroot/html/js/liferay/pagination.js
@@ -31,6 +31,8 @@ AUI.add(
 
 		var STR_STRINGS = 'strings';
 
+		var LOCALE_RU = 'ru_RU';
+
 		var Pagination = A.Component.create(
 			{
 				ATTRS: {
@@ -76,13 +78,13 @@ AUI.add(
 							return A.merge(
 								value,
 								{
-									items: themeDisplay.getLanguageId() == 'ru_RU' ? '' : Liferay.Language.get('items'),
+									items: themeDisplay.getLanguageId() == LOCALE_RU ? '' : Liferay.Language.get('items'),
 									next: Liferay.Language.get('next'),
 									of: Liferay.Language.get('of'),
-									page: themeDisplay.getLanguageId() == 'ru_RU' ? Liferay.Language.get('page-dp') : Liferay.Language.get('page'),
+									page: themeDisplay.getLanguageId() == LOCALE_RU ? Liferay.Language.get('page-dp') : Liferay.Language.get('page'),
 									per: Liferay.Language.get('per'),
 									prev: Liferay.Language.get('previous'),
-									results: themeDisplay.getLanguageId() == 'ru_RU' ? '' : Liferay.Language.get('results'),
+									results: themeDisplay.getLanguageId() == LOCALE_RU ? '' : Liferay.Language.get('results'),
 									showing: Liferay.Language.get('showing')
 								}
 							);
